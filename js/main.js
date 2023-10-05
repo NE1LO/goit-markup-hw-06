e0c34c7e97a24b3ce52a1784a5faf236a70842fe
Anton <asmeiko13@gmail.com>
2023-10-05 14:45:44 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-mobile-menu-open').onclick = function() {
+document.getElementById('block-mobile-menu').classList.add('is-open')
+}
+
+document.getElementById('btn-close-menu').onclick = function() {
+  document.getElementById('block-mobile-menu').classList.remove('is-open')
+  }
