4a6b1febec83da6e09d2e5b403ad9fdd21308787
Anton <asmeiko13@gmail.com>
2023-10-31 11:06:30 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-document.getElementById('btn-mobile-menu-open').onclick = function() {
-document.getElementById('block-mobile-menu').classList.add('is-open')
+// open-burger-menu
+document.getElementById('btn-mobile-menu-open').onclick = function () {
+  document.getElementById('block-mobile-menu').classList.add('is-open');
+};
+
+document.getElementById('btn-close-menu').onclick = function () {
+  document.getElementById('block-mobile-menu').classList.remove('is-open');
+};
+
+// open-modal-window
+const openWindow = document.getElementById('btn-open-modal');
+const closeWindow = document.getElementById('btn-close-modal');
+const modalWindow = document.getElementById('modal-window');
+
+function openModal() {
+  modalWindow.classList.add('is-open-modal');
 }
 
-document.getElementById('btn-close-menu').onclick = function() {
-  document.getElementById('block-mobile-menu').classList.remove('is-open')
-  }
+function closeModal() {
+  modalWindow.classList.remove('is-open-modal');
+}
+
+openWindow.addEventListener('click', openModal);
+closeWindow.addEventListener('click', closeModal);
